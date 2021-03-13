6ac99108cdd3db916c9a282adf380e3b8784dc68
Himanshu Gautam <1302399+hgautam@users.noreply.github.com>
2021-03-13 13:11:14 -0800

    Create simple-map.go

---
+++ Hash-tables/simple-map.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	teams := map[string]int{
+		"HTML":   3,
+		"PYTHON": 6,
+		"GO":     21,
+		"JAVA":   18,
+	}
+
+	//iterate over the keys and values
+	for key, value := range teams {
+		fmt.Println(key, value)
+	}
+
+	//search for a key
+	if val, ok := teams["GO"]; ok {
+		//do something here
+		fmt.Println("found", val)
+		teams["GO"] = 27
+	}
+
+	//iterate over the keys and values
+	for key, value := range teams {
+		fmt.Println(key, value)
+	}
+}
