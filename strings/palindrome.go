478854069acd9b4f9a255aef9672bba51353c868
Himanshu Gautam <1302399+hgautam@users.noreply.github.com>
2021-03-23 11:38:22 -0700

    Create palindrome.go

---
+++ strings/palindrome.go
@@ -0,0 +1,28 @@
+/*
+Checks if a string is a palindrome
+input "abcdcba" will return in true
+time complexity: o(n)
+space complexity: o(1)
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	str := "abcdcba"
+
+	isPalindrome := true
+	j := len(str) - 1
+	for i := 0; i < len(str); i++ {
+		if string(str[i]) != string(str[j]) {
+			fmt.Println("strig is not a palindrome")
+			isPalindrome = false
+			break
+		}
+		j = j - 1
+	}
+	fmt.Println("is this a palindrome: ", isPalindrome)
+}
