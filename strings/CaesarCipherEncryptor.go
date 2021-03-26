20ce7227c13ab705b67db32b9051ea6dc4e860a1
Himanshu Gautam <1302399+hgautam@users.noreply.github.com>
2021-03-26 16:11:13 -0700

    Update CaesarCipherEncryptor.go

--- strings/CaesarCipherEncryptor.go
+++ strings/CaesarCipherEncryptor.go
@@ -1,7 +1,6 @@
 /*
 Returns a string based on a key
 e.g. given key = 2, "xyz" input will result in "zab" output
-Time efficieny should be o(n)
 */
 package main
 
@@ -19,13 +18,30 @@ func main() {
 		"d": 4,
 		"e": 5,
 		"f": 6,
+		"g": 7,
+		"h": 8,
+		"i": 9,
+		"j": 10,
+		"k": 11,
+		"l": 12,
+		"m": 13,
+		"n": 14,
+		"o": 15,
+		"p": 16,
+		"q": 17,
+		"r": 18,
+		"s": 19,
+		"t": 20,
+		"u": 21,
+		"v": 22,
+		"w": 23,
 		"x": 24,
 		"y": 25,
 		"z": 26,
 	}
 	var sb strings.Builder
 	str := "xyz"
-	myKey := 2
+	key := 5
 	fmt.Println("Input string is: ", str)
 	for i := 0; i < len(str); i++ {
 		charVal := string(str[i])
@@ -33,7 +49,7 @@ func main() {
 		value := alphaMap[charVal]
 		//fmt.Println("value for string is: ", value)
 
-		myRealKey := value + myKey
+		myRealKey := value + key
 
 		if myRealKey > 26 {
 			myRealKey = myRealKey % 26
@@ -41,12 +57,12 @@ func main() {
 
 		//find a value in the map and print its key
 
-		for key, val := range alphaMap {
-			//fmt.Println("Key:", key, "=>", "value:", val)
+		for alpahbet, val := range alphaMap {
+			//fmt.Println("Alphabet:", alpahbet, "=>", "value:", val)
 			if val == myRealKey {
 				//fmt.Println("found")
-				//fmt.Println("Return string is: ", key)
-				sb.WriteString(key)
+				//fmt.Println("Return string is: ", alpahbet)
+				sb.WriteString(alpahbet)
 				break
 			}
 		}
